9e3684c9d111386d7b99bfbce957128200b7ba75
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-10-18 01:20:53 +0530

    Updates

---
+++ precache-manifest.a40126224e956a6bfc6f240fa28fec13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d414841afb6b3c867152",
+    "url": "/balajistores/static/js/runtime~main.d414841a.js"
+  },
+  {
+    "revision": "1a88269f6b80e1dceed9",
+    "url": "/balajistores/static/js/main.1a88269f.chunk.js"
+  },
+  {
+    "revision": "0e7c6a2c90d5db3eb37a",
+    "url": "/balajistores/static/js/1.0e7c6a2c.chunk.js"
+  },
+  {
+    "revision": "1a88269f6b80e1dceed9",
+    "url": "/balajistores/static/css/main.bc22d66b.chunk.css"
+  },
+  {
+    "revision": "92f1c1361167a70a3c2bf83db117b0d1",
+    "url": "/balajistores/index.html"
+  }
+];
